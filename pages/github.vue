c6f0a92ef31d72f8e86e338d07b254f3e747ec53
Wuemeli <wuemeli@gmail.com>
2023-10-01 09:39:23 +0200

    Improves the Page Loading Speed 🔥

--- pages/github.vue
+++ pages/github.vue
@@ -3,12 +3,12 @@
     <div class="flex flex-col items-center">
       <p class="font-semibold">My Github Stats 💻</p>
       <br />
-      <img :src="githubStats" alt="Github Stats" class="w-30 h-auto" />
+      <img src="https://github-readme-stats.vercel.app/api?username=wuemeli&show_icons=true&theme=radical&border=false" alt="Github Stats" class="w-30 h-auto" />
     </div>
     <div class="flex flex-col items-center">
       <p class="font-semibold">My Github Streak 🔥</p>
       <br />
-      <img :src="githubStreak" alt="Github Streak" class="w-30 h-auto" />
+      <img src="https://github-readme-streak-stats.herokuapp.com/?user=Wuemeli&theme=radical&border=false" alt="Github Streak" class="w-30 h-auto" />
     </div>
   </div>
 </template>
@@ -18,27 +18,6 @@ export default {
   name: 'GithubPage',
   head: {
     title: 'Github Stats 💻',
-  },
-  data() {
-    return {
-      githubStats: '',
-      githubStreak: '',
-    }
-  },
-  methods: {
-    async getGithubStats() {
-      try {
-        this.githubStats =
-          'https://github-readme-stats.vercel.app/api?username=wuemeli&show_icons=true&theme=radical&border=false'
-        this.githubStreak =
-          'https://github-readme-streak-stats.herokuapp.com/?user=Wuemeli&theme=radical&border=false'
-      } catch (error) {
-        console.error('Error fetching Github stats:', error)
-      }
-    },
-  },
-  mounted() {
-    this.getGithubStats()
   },
 }
 </script>
