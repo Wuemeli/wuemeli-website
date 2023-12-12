2dd0ed07e7a02046329544b241548f45736798c0
Wuemeli <wuemeli@gmail.com>
2023-12-12 10:27:20 +0100

    Remove Spotify

--- server/plugins/imagePreloader.js
+++ server/plugins/imagePreloader.js
@@ -5,7 +5,6 @@ export default function ({ app }, inject) {
       'https://github-readme-streak-stats.herokuapp.com/?user=Wuemeli&theme=radical&border=false',
       'https://dcbadge.vercel.app/api/shield/704918773035171931?theme=gray&logoColor=presence',
       'https://github-readme-stats.vercel.app/api/wakatime?username=wuemeli&theme=radical',
-      'https://spotify-github-profile.vercel.app/api/view?uid=d1ytrh3gx9xa7mzw79h2mbeqi&cover_image=true&theme=novatorem&show_offline=false&background_color=121212&interchange=false&bar_color=53b14f&bar_color_cover=false'
     ];
 
     images.forEach(src => {
