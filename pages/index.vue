03e0332493333ac7afdaf587896982c3b974f593
Wuemeli <wuemeli@gmail.com>
2024-01-08 12:43:43 +0100

    Adds a Waving Hand

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="max-w-4xl px-10 mx-auto space-y-8 text-center animate-fade-up">
     <div class="flex flex-col items-center">
-      <p class="text-light-gray">Hi 👋 , my name is</p>
+      <p class="text-light-gray flex items-center">
+        Hi <span><img src="/icons/Hi.gif" alt="wave" width="20px" height="20px"></span>, my name is
+      </p>
       <div class="flex items-center">
-        <p class="text-4xl font-semibold">Wuemeli</p>
+        <p class="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-semibold">Wuemeli</p>
       </div>
-      <p class="text-lg font-semibold text-light-gray">
+      <p class="text-sm sm:text-md md:text-lg lg:text-xl font-semibold text-light-gray">
         Full Stack Developer specializing in Discord Bots and Websites
       </p>
       <p class="text-light-gray">
-        I  ❤️ creating Discord Bots and Websites
+        I ❤️ creating Discord Bots and Websites
       </p>
     </div>
   </div>
