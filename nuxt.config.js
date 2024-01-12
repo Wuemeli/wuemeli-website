0998aa60509d1e1f35a462d0435c398d60176cba
Wuemeli <wuemeli@gmail.com>
2024-01-12 10:33:56 +0100

    Fix SEO

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,21 +8,21 @@ export default defineNuxtConfig({
       title: 'Wuemeli\'s Portfolio 🔥',
       htmlAttrs: {
         lang: 'en'
-      }
+      },
+      meta: [
+        { hid: 'description', name: 'description', content: 'Explore Wuemeli\'s impressive portfolio of web development projects, including Nuxt.js and Discord.js creations. Discover innovative web solutions and creative designs.' },
+        { name: 'keywords', content: 'portfolio, web development, Nuxt.js, Discord.js, Wuemeli, projects, creative designs, web solutions' },
+        { name: 'author', content: 'Wuemeli' },
+        { hid: 'og:title', property: 'og:title', content: 'Wuemeli\'s Portfolio 🔥' },
+        { hid: 'og:description', property: 'og:description', content: 'Explore Wuemeli\'s impressive portfolio of web development projects, including Nuxt.js and Discord.js creations. Discover innovative web solutions and creative designs.' },
+        { hid: 'og:url', property: 'og:url', content: 'https://wuemeli.com' },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:image', content: 'https://wuemeli.com/img/og-image.png' },
+        { name: 'twitter:card', content: 'summary_large_image' },
+        { name: 'twitter:title', content: 'Wuemeli\'s Portfolio' },
+        { name: 'twitter:description', content: 'Explore Wuemeli\'s web development portfolio, including Nuxt.js and Discord.js projects. Get inspired by innovative solutions and creative designs.' },
+      ],
     },
-    meta: [
-      { hid: 'description', name: 'description', content: 'Explore Wuemeli\'s impressive portfolio of web development projects, including Nuxt.js and Discord.js creations. Discover innovative web solutions and creative designs.' },
-      { name: 'keywords', content: 'portfolio, web development, Nuxt.js, Discord.js, Wuemeli, projects, creative designs, web solutions' },
-      { name: 'author', content: 'Wuemeli' },
-      { hid: 'og:title', property: 'og:title', content: 'Wuemeli\'s Portfolio 🔥' },
-      { hid: 'og:description', property: 'og:description', content: 'Explore Wuemeli\'s impressive portfolio of web development projects, including Nuxt.js and Discord.js creations. Discover innovative web solutions and creative designs.' },
-      { hid: 'og:url', property: 'og:url', content: 'https://wuemeli.com' },
-      { property: 'og:type', content: 'website' },
-      { property: 'og:image', content: 'https://wuemeli.com/img/og-image.png' },
-      { name: 'twitter:card', content: 'summary_large_image' },
-      { name: 'twitter:title', content: 'Wuemeli\'s Portfolio' },
-      { name: 'twitter:description', content: 'Explore Wuemeli\'s web development portfolio, including Nuxt.js and Discord.js projects. Get inspired by innovative solutions and creative designs.' },
-    ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
