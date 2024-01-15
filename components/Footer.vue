328a3b4a7a74eb7e6e2d995a64a2a2f6aa089fa6
Wuemeli <wuemeli@gmail.com>
2024-01-15 12:03:34 +0100

    sss

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,13 +1,7 @@
 <template>
   <div class="flex flex-col-reverse items-center justify-between px-10 py-3 text-center bg-iridium lg:flex-row">
-    <p class="mt-2 text-light-gray flex items-center">
-      <span>Made with</span>
-      <span class="text-red-500 ml-2 mr-2">❤</span>
-      <span>by Wuemeli.</span>
-      <span class="ml-2">With</span>
-      <span><img src="https://wakatime.com/badge/user/1b863d20-30af-46ca-add5-692804513d23.svg" alt="Coding Time"
-          class="ml-2" /></span>
-      <span class="ml-2">coding time.</span>
+    <p class="mt-2 text-light-gray lg:mt-0">
+      Made with <span class="text-red-500">❤</span> by Wuemeli, Jay-Are Ocero
     </p>
     <div class="flex lg:hidden"></div>
   </div>
