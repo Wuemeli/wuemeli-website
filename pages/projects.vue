48d073000bc131b1ac51d286b4a77cca44ab86ad
Wuemeli <wuemeli@gmail.com>
2024-02-09 01:38:57 +0100

    Fix

--- pages/projects.vue
+++ pages/projects.vue
@@ -9,6 +9,11 @@
           <div class="max-w-sm mx-auto">
             <h2 class="text-2xl font-bold text-iridium">{{ project.title }}</h2>
             <p class="text-gray-600">{{ truncateDescription(project.description) }}</p>
+            <div class="flex justify-between items-center mt-4">
+              <a :href="project.html_url" target="_blank" rel="noopener noreferrer"
+                class="inline-block bg-dune text-white px-6 py-3 rounded transform transition duration-500 ease-in-out hover:-translate-y-1 hover:scale-110">Check
+                it Out</a>
+            </div>
           </div>
         </div>
       </div>
@@ -48,7 +53,14 @@ export default {
           id: 1,
           title: 'Heckerbot',
           description: 'Heckerbot is a advanced Discord bot with a lot of features.',
+          html_url: 'https://heckerbot.dev/',
         },
+        {
+          id: 2,
+          title: 'TOR Relay Configurator',
+          description: 'A simple TOR Relay Configurator for Linux.',
+          html_url: 'https://tor-relay.dev/',
+        }
       ],
     }
   },
