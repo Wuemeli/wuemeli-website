f04d782df226f1ac43e7ffaae5ccdfb8d7eb559c
Wuemeli <wuemeli@gmail.com>
2024-02-27 21:47:01 +0100

    Makes the Footer more beautiful

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="flex flex-col-reverse items-center justify-between px-10 py-3 text-center bg-iridium lg:flex-row">
+  <div class="flex flex-col-reverse items-center justify-center px-10 py-3 text-center lg:flex-row">
     <p class="mt-2 text-light-gray lg:mt-0">
-      Made with <span class="text-red-500">❤</span> by Wuemeli
+      Made with <span class="text-red-500">❤</span>  by Wuemeli
     </p>
     <div class="flex lg:hidden"></div>
   </div>
-</template>
+</template>x
