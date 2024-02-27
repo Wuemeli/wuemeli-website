e0f6181a53971b36149e8c2cfe4398d7a2f80e68
Wuemeli <wuemeli@gmail.com>
2024-02-27 13:41:31 +0000

    Upsie Dupsie Fix

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="max-w-4xl px-10 mx-auto space-y-8 text-center animate-fade-up">
     <div class="flex flex-col items-center">
       <span class="type-hello text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-semibold text-light-gray"></span>
-      <span class="text-1xl sm:text-3xl md:text-4xl lg:text-0xl font-semibold text-dark-gray">im Wuemeli</span>
+      <span class="text-1xl sm:text-3xl md:text-4xl lg:text-0xl font-semibold text-light-gray">im Wuemeli</span>
       <p class="mt-2 text-lg sm:text-xl md:text-2xl lg:text-3xl font-light text-light-gray"></p>
     </div>
     <div class="flex flex-col items-center">
