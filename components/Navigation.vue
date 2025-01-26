98e69e94631c46ce15a17a82cb5235fecf236967
Wuemeli <54443643+Wuemeli@users.noreply.github.com>
2025-01-26 17:55:47 +0100

    Little Update. Still need to fix the Design

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,57 +1,107 @@
 <template>
-    <div>
-      <nav class="flex flex-col md:flex-row items-center justify-between px-4 md:px-10 py-2 bg-iridium">
-        <div class="flex-row items-center space-x-3 sm:flex">
-          <div class="button w-3 h-3 rounded-full animate_animated animate__bounceIn bg-mac-red" @click="exitFullscreen">
-          </div>
-          <div class="button w-3 h-3 rounded-full animate_animated animate__bounceIn bg-mac-yellow" ref="yellowButton"
-            @click="changeColor"></div>
-          <div class="button w-3 h-3 rounded-full animate_animated animate__bounceIn bg-mac-green" @click="enterFullscreen">
-          </div>
-        </div>
-        <div class="flex-grow flex items-center justify-center">
-          <p class="animate_animated animate__bounceIn text-center md:text-left">
-            {{ newroutename }} - Wuemelis Portfolio {{ getTimezoneStatus() }}
-          </p>
-        </div>
-        <img src="https://dcbadge.limes.pink/api/shield/704918773035171931?theme=gray&logoColor=presence"
-          alt="Discord Status" width="250" class="animate_animated animate__bounceIn"
-          onclick="window.open('https://discord.com/users/704918773035171931', '_blank')">
-      </nav>
-      <div class="flex flex-col md:flex-row bg-baltic-sea">
-        <div class="flex flex-col md:flex-row items-center px-4 md:px-10 py-3 space-x-0 md:space-x-7 bg-dark-gray">
-        <router-link to="/" class="text-red-500 hover:bg-gray-800 animated-link" active-class="text-blue-500 font-bold"
-          exact>
+  <div>
+    <nav
+      class="flex flex-col md:flex-row items-center justify-between px-4 md:px-10 py-2 bg-iridium"
+    >
+      <div class="flex-row items-center space-x-3 sm:flex">
+        <div
+          class="button w-3 h-3 rounded-full animate_animated animate__bounceIn bg-mac-red"
+          @click="exitFullscreen"
+        ></div>
+        <div
+          class="button w-3 h-3 rounded-full animate_animated animate__bounceIn bg-mac-yellow"
+          ref="yellowButton"
+          @click="changeColor"
+        ></div>
+        <div
+          class="button w-3 h-3 rounded-full animate_animated animate__bounceIn bg-mac-green"
+          @click="enterFullscreen"
+        ></div>
+      </div>
+      <div class="flex-grow flex items-center justify-center">
+        <p class="animate_animated animate__bounceIn text-center md:text-left">
+          {{ newroutename }} - Wuemelis Portfolio
+        </p>
+      </div>
+      <p
+        class="animate_animated animate__bounceIn text-center md:text-right"
+        onclick="window.open('https://blog.wuemeli.com', '_blank')"
+      >
+        📖 Read my blog
+      </p>
+    </nav>
+    <div class="flex flex-col md:flex-row bg-baltic-sea">
+      <div
+        class="flex flex-col md:flex-row items-center px-4 md:px-10 py-3 space-x-0 md:space-x-7 bg-dark-gray"
+      >
+        <router-link
+          to="/"
+          class="text-red-500 hover:bg-gray-800 animated-link"
+          active-class="text-blue-500 font-bold"
+          exact
+        >
           <div class="flex items-center mr-3">
-            <NuxtImg src="/icons/vue.svg" alt="Vue icon" class="w-5 h-auto mr-2" />
+            <NuxtImg
+              src="/icons/vue.svg"
+              alt="Vue icon"
+              class="w-5 h-auto mr-2"
+            />
             <p>Index.vue</p>
           </div>
         </router-link>
-        <router-link to="/experience" class="text-red-500 hover:bg-gray-800 animated-link"
-          active-class="text-blue-500 font-bold" exact>
+        <router-link
+          to="/experience"
+          class="text-red-500 hover:bg-gray-800 animated-link"
+          active-class="text-blue-500 font-bold"
+          exact
+        >
           <div class="flex items-center mr-3">
-            <NuxtImg src="/icons/js.svg" alt="JS icon" class="w-5 h-auto mr-2" />
+            <NuxtImg
+              src="/icons/js.svg"
+              alt="JS icon"
+              class="w-5 h-auto mr-2"
+            />
             <p>Experience.js</p>
           </div>
         </router-link>
-        <router-link to="/projects" class="text-red-500 hover:bg-gray-800 animated-link"
-          active-class="text-blue-500 font-bold" exact>
+        <router-link
+          to="/projects"
+          class="text-red-500 hover:bg-gray-800 animated-link"
+          active-class="text-blue-500 font-bold"
+          exact
+        >
           <div class="flex items-center">
             <img src="/icons/ts.svg" alt="TS icon" class="w-5 h-auto mr-2" />
             <p>Projects.ts</p>
           </div>
         </router-link>
-        <router-link to="/codingstats" class="text-red-500 hover:bg-gray-800 animated-link"
-          active-class="text-blue-500 font-bold" exact>
+        <router-link
+          to="/codingstats"
+          class="text-red-500 hover:bg-gray-800 animated-link"
+          active-class="text-blue-500 font-bold"
+          exact
+        >
           <div class="flex items-center">
-            <img src="/icons/python.svg" alt="Python icon" class="w-5 h-auto mr-2" />
+            <img
+              src="/icons/python.svg"
+              alt="Python icon"
+              class="w-5 h-auto mr-2"
+            />
             <p>Coding Stats.py</p>
           </div>
         </router-link>
-        <router-link to="/github" class="text-red-500 hover:bg-gray-800 animated-link"
-          active-class="text-blue-500 font-bold" exact>
+        <router-link
+          to="/github"
+          class="text-red-500 hover:bg-gray-800 animated-link"
+          active-class="text-blue-500 font-bold"
+          exact
+        >
           <div class="flex items-center">
-            <img src="/icons/github.svg" alt="GitHub icon" class="w-5 h-auto mr-2" />
+            <img
+              src="/icons/github.svg"
+              alt="GitHub icon"
+              class="w-5 h-auto mr-2"
+            />
             <p>Github.gh</p>
           </div>
         </router-link>
@@ -61,8 +111,6 @@
 </template>
 
 <script>
-import moment from 'moment-timezone'
-
 export default {
   setup() {
     const route = useRoute()
@@ -105,27 +153,17 @@ export default {
         document.msExitFullscreen()
       }
     },
-    getTimezoneStatus() {
-      const time = moment().tz('Europe/Zurich')
-      const hour = time.hour()
-
-      if (hour >= 22 || hour < 6) {
-        return '🌙'
-      } else {
-        return '🌞'
-      }
+    mounted() {
+      const animatedLinks = document.querySelectorAll('.animated-link')
+      animatedLinks.forEach((link) => {
+        link.addEventListener('mouseenter', () => {
+          link.classList.add('animated-link-hover')
+        })
+        link.addEventListener('mouseleave', () => {
+          link.classList.remove('animated-link-hover')
+        })
+      })
     },
-  },
-  mounted() {
-    const animatedLinks = document.querySelectorAll('.animated-link')
-    animatedLinks.forEach((link) => {
-      link.addEventListener('mouseenter', () => {
-        link.classList.add('animated-link-hover')
-      })
-      link.addEventListener('mouseleave', () => {
-        link.classList.remove('animated-link-hover')
-      })
-    })
   },
 }
 </script>
--- content/blog/abuseipdb.md
+++
@@ -1,8 +0,0 @@
----
-title: Abuseipdb Badge
-author: wuemeli
-description: >
-  Just my badge for abuseipdb.
----
-
-![Abuseipdb Badge](https://www.abuseipdb.com/contributor/139912.svg)
