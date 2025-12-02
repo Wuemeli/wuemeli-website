9ef2f23b5c290924ba03a83c3bcbd2f52a89ebef
wuemeli <github@wuemeli.com>
2025-12-02 10:43:15 +0100

    wowie

--- content.config.ts
+++ content.config.ts
@@ -1,19 +1,16 @@
 import { defineCollection, defineContentConfig, z } from '@nuxt/content'
-import { asSitemapCollection } from '@nuxtjs/sitemap/content'
 
 export default defineContentConfig({
   collections: {
-    blog: defineCollection(
-      asSitemapCollection({
-        type: 'page',
-        source: 'blog/*.md',
-        schema: z.object({
-          title: z.string(),
-          description: z.string(),
-          date: z.string(),
-          slug: z.string().optional(),
-        }),
+    blog: defineCollection({
+      type: 'page',
+      source: 'blog/*.md',
+      schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        date: z.string(),
+        slug: z.string().optional(),
       }),
-    ),
+    }),
   },
 })
